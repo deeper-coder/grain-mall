cfd8816449c290274ee4a7bd89738044d1ec437f
Pluto Evans <deep_coder@hotmail.com>
2024-02-23 18:42:10 +0800

    项目初始结构创建

---
+++ grainmall-order/src/test/java/xyz/deepcoder/grainmall/order/GrainmallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.deepcoder.grainmall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrainmallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ grainmall-ware/src/main/java/xyz/deepcoder/grainmall/ware/GrainmallWareApplication.java
@@ -0,0 +1,13 @@
+package xyz.deepcoder.grainmall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrainmallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrainmallWareApplication.class, args);
+    }
+
+}
